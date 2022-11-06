73a6a010f00b1aba8971dfcaadcf33576f6840a2
Neka <nekoanchor@yahoo.fr>
2022-11-06 23:51:38 +0100

    docs: update content landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -38,15 +38,66 @@ const LandingPage = () => {
 
   return (
     <section className={styles.__landing_page}>
-      <Navbar links={[{ target: "#banner1", name: "Banner1"}, { target: "#banner2", name: "Banner2"}, { target: "#banner3", name: "Banner3"}, { target: "#contact", name: "Contact"}]} />
-      <Hero image={hero} title={["The time-saver" ,"for teams & companies"]} alt={"group of people sitting while using laptop computer"} button={"CallToAction"}/>
+      <Navbar
+        links={[
+          { target: "#liberte", name: "Liberté" },
+          { target: "#management", name: "Management" },
+          { target: "#teletravail", name: "Télétravail" },
+          { target: "#newsletters", name: "Newsletters" },
+        ]}
+      />
+      <Hero
+        image={hero}
+        title={["The time-saver", "for teams & companies"]}
+        alt={"group of people sitting while using laptop computer"}
+        button={"Contactez-nous"}
+      />
       <div className={styles.__content}>
-        {[{ image: banner1, alt: "man and woman sitting in front of silver macbook", orientation: "left", anchor: "banner1" }, { image: banner2, alt: "three person pointing the silver laptop computer", orientation: "right", anchor: "banner2" }, { image: banner3, alt: "black iPad beside cup of coffee on table", orientation: "left", anchor: "banner3" }].map(
-          (banner) => (
-            <Banner anchor={banner.anchor} orientation={banner.orientation} image={banner.image} alt={banner.alt} key={uuidv4()} title={"Fusce qui nulla"} content={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus harum laborum, recusandae debitis, consectetur ex neque voluptas culpa mollitia illo molestiae dignissimos eveniet provident rerum deleniti porro voluptatum blanditiis vitae numquam! Quaerat, sapiente! Ratione nihil quam ut porro fugiat reiciendis. Minima ea, nam neque tempora similique unde sint perspiciatis sunt provident praesentium. Quae veritatis rem, similique unde deserunt dicta velit repellat delectus. Omnis a eveniet quis impedit asperiores voluptas, cum suscipit dignissimos blanditiis vel quaerat exercitationem, sit earum enim aliquid. Repudiandae ex beatae maxime tenetur deleniti sunt odio ducimus alias reiciendis, et eius id nihil at possimus, modi, iusto totam corrupti voluptatum inventore? Hic voluptatum sed distinctio blanditiis quia explicabo? Ducimus ea mollitia minus, consectetur impedit obcaecati cupiditate autem laboriosam neque deserunt expedita ullam quasi ratione fugit animi hic, consequatur eaque tenetur tempora quia unde provident? Hic, nihil iure voluptatem incidunt officiis molestias, delectus rem quas quibusdam, esse est! Sed."}/>
-          )
-        )}
-        <Newsletter anchor={"contact"} title={"Fusce qui nulla"} lead={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam quis facere accusantium itaque, dignissimos ipsam amet cumque nobis iste possimus dolore."} content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aspernatur ad voluptates suscipit eaque libero omnis et repudiandae exercitationem. Eos quas accusantium dicta a culpa."}/>
+        {[
+          {
+            image: banner1,
+            alt: "man and woman sitting in front of silver macbook",
+            orientation: "left",
+            anchor: "liberte",
+            title: "La liberté, une motivation.",
+            content: "...",
+          },
+          {
+            image: banner2,
+            alt: "three person pointing the silver laptop computer",
+            orientation: "right",
+            anchor: "management",
+            title: "Le Management, une priorité.",
+            content: "...",
+          },
+          {
+            image: banner3,
+            alt: "black iPad beside cup of coffee on table",
+            orientation: "left",
+            anchor: "teletravail",
+            title: "Le télétravail, le futur.",
+            content:
+              "Le télétravail c'est le futur parce que c'est un moyen de travailler plus efficacement et de manière plus flexible. Il permet aux gens de travailler à leur propre rythme et de se concentrer sur leur travail sans les distractions de l'open-space. Le télétravail est de plus en plus courant, notamment dans les pays développés. En effet, les avancées technologiques ont permis de créer des outils qui facilitent le travail à distance. De plus, de nombreuses entreprises ont compris que le télétravail permet de réduire les coûts liés aux bureaux et au personnel. Enfin, le télétravail permet aux salariés de gagner du temps en évitant les trajets domicile-travail.",
+          },
+        ].map((banner) => (
+          <Banner
+            anchor={banner.anchor}
+            orientation={banner.orientation}
+            image={banner.image}
+            alt={banner.alt}
+            key={uuidv4()}
+            title={banner.title}
+            content={banner.content}
+          />
+        ))}
+        <Newsletter
+          anchor={"newsletters"}
+          title={"Newsletters"}
+          lead={
+            "Inscrivez-vous aux newsletters de Savime pour recevoir les mails d'informations concernant les futures fonctionnalités."
+          }
+          content={"Vous recevrez un mail d'informations par mois."}
+        />
       </div>
       <Footer />
     </section>
