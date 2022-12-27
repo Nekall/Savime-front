7c293d58334045fec35d1e9e99d75b0a1a30b29d
Neka <nekoanchor@yahoo.fr>
2022-12-27 18:53:41 +0100

    feat: add v1 of loading page~

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,13 @@
+// Assets
+import logo from "../../assets/images/logo/Logo-v1.png"
+
 // Styles
 import styles from "./styles.module.scss"
 
 const Loading = () => {
     return(
         <div className={styles.__loading}>
-            <div>Chargement en cours...</div>
+            <img className={styles.__animation} src={logo} alt="Logo de chargement" />
         </div>
     )
 }
